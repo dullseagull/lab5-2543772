6cb7ffbb93cee23277f7ca74b7521cca37927876
dullseagull <2543772@students.wits.ac.za>
2024-04-08 14:34:14 +0000

    msg

--- api/message/index.js
+++ api/message/index.js
@@ -51,8 +51,3 @@ app.listen(3001, () => {
     console.log('Server started at http://localhost:3001');
 });
 
-module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
-};
