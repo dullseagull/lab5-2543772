f1747c3eb40d21f5f8dc1287e8c991cb2ad0672c
dullseagull <2543772@students.wits.ac.za>
2024-04-09 20:08:55 +0200

    edit

--- api/src/functions/script.js
+++ api/src/functions/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const carList = document.getElementById('carList');
     cars = [];
     loadCarsBtn.addEventListener('click', () => {
-        fetch('api/cars')
+        fetch('api/functions')
             .then(response => response.json())
             .then(data => {
                 cars = data;
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 function addCar(newCar) {
-    fetch('api/cars', {
+    fetch('api/functions', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -60,7 +60,7 @@ carForm.addEventListener('submit', event => {
 // Function to remove a car
 function removeCar(index) {
     const carId = cars[index].id;
-    fetch(`api/cars/${carId}`, {
+    fetch(`api/functions/${carId}`, {
         method: 'DELETE'
     })
         .then(response => response.json())
